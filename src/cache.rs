34f6170c77928208a392b82fefe4a0126faefe30
Greg Whiteley <greg.whiteley@gmail.com>
2025-02-11 16:55:24 +1100

    cache: add json rendering tests

--- src/cache.rs
+++ src/cache.rs
@@ -7,12 +7,19 @@ use tokio::io::AsyncReadExt;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Default)]
+/// Wrap cache entries in version number we so we can be incompatible later
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub(crate) enum CacheVersions {
+    #[serde(rename = "v1")]
+    V1(Cache),
+}
+
+#[derive(Serialize, Deserialize, Debug, Default, PartialEq)]
 pub(crate) struct Cache {
     pub files: Vec<File>,
 }
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
 pub(crate) struct File {
     pub path: String,
     pub object: String,
@@ -34,4 +41,33 @@ pub(crate) async fn read_hash(path: &async_std::path::Path, len: &Option<u64>) -
     }
     let result = sha.finalize();
     Ok(result.into())
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_compat() {
+        let cache: Cache = serde_json::from_str("{ \"files\": [] }").unwrap();
+        assert!(cache.files.is_empty());
+        let cache: Cache = serde_json::from_str(r#"{ "files": [], "else": 1 }"#).unwrap();
+        assert!(cache.files.is_empty(), "Expected to ignore additional fields");
+
+        // Round trip of version container
+        let mut c = Cache::default();
+        c.files.push(File{ path: "foo.exe".into(), object: "aa/bb/cc/dddd".into(), size: 123456 });
+        let v = CacheVersions::V1(c);
+        let x = serde_json::to_string(&v).unwrap();
+
+        let inp: CacheVersions = serde_json::from_str(r#" {
+"v1": {
+    "files":[{"path":"foo.exe","object":"aa/bb/cc/dddd","size":123456}]
+  }
+}"#).unwrap();
+        assert_eq!(inp, v);
+        assert_eq!(serde_json::from_str::<CacheVersions>(&x).unwrap(), v);
+    }
+
 }
